238c8d097c2f7c56dc2891a0588ce15bb571a9d9
Kirill Kuznetsov <kuznechik.86@mail.ru>
2020-08-16 23:37:49 +0300

    Что-то потеряно

--- common_files/src/main/java/com/geekbrains/common_files/common/Config.java
+++ common_files/src/main/java/com/geekbrains/common_files/common/Config.java
@@ -4,5 +4,5 @@ public interface Config {
     byte SIGNAL_UPLOAD = 25;
     byte SIGNAL_DOWNLOAD = 17;
     byte SIGNAL_UPDATE = 15;
-    String PATH_SERVER="./common/src/main/resources/serverFiles/";
+    String PATH_SERVER="./common_files/src/main/resources/serverFiles/";
 }
